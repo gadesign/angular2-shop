96824759dd188f4f8e53283fdf87a80df8e8929c
Jeremy Smith <goodanswerdesign@gmail.com>
2016-04-13 08:13:32 -0600

    Add an image toggle

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -7,8 +7,9 @@ import { Component } from 'angular2/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
-    imageWidth: number = 50;
+    imageWidth: number = 100;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
         "productId": 1,
@@ -61,4 +62,8 @@ export class ProductListComponent {
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
     ];
+    
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
