8c9710c6e8705964d6d7e18156ec4b3de3c347b0
Jeremy Smith <goodanswerdesign@gmail.com>
2016-04-15 18:28:18 -0600

    Add transform pipe for product list with external css.

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,17 +1,21 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe]
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 100;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -66,5 +70,9 @@ export class ProductListComponent {
     
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+    
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
