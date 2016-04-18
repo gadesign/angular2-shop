0e07335dfd19657edcd2d5662d918c41309dd891
Jeremy Smith <goodanswerdesign@gmail.com>
2016-04-18 07:40:13 -0600

    Add Product Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from 'angular2/core';
-import { ProductListComponent } from './products/product-list.component'
+import { ProductListComponent } from './products/product-list.component';
+import { ProductService } from './products/product.service';
 @Component({
     selector:'pm-app',
     template:`<div><h1>{{ pageTitle }}</h1></div>
     <pm-products></pm-products>
     `,
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService]
 })
 export class AppComponent {
     pageTitle: string = 'Good Answer Design';
